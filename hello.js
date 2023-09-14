9fc122c9eb1bb65ab5c83e4da8659378964b1f64
Vance <vancesutherland@hotmail.com>
2023-09-14 18:46:50 -0500

    hello

--- hello.js
+++ hello.js
@@ -1,13 +1,26 @@
-let myName = input.question("What's your name? ");
-let myAge = Number(input.question("\nHow old are you? "));
-let myPhrase;
+const input = require('readline-sync');
 
-if (myAge < 18) {
-    myPhrase = "Parental supervision advised.";
-} else if (myAge < 26) {
-    myPhrase = "Imagine paying for health insurance...";
+// instantiate variables 
+let hoursInWeek = 168;
+
+let sleepHours = 56;
+let workHours = 40;
+let miscHours = 21;
+let studyHours = 18;
+
+// create variable to reduce clutter
+let necessities = sleepHours + workHours + miscHours + studyHours;
+
+// instantiate hours spent gaming on teh daily, then multiply by 7 to update leisureHours
+let dailyGaming = Number(input.question("How many hours a day would you like to spend playing Baldur's Gate 3? "));
+let leisureHours = dailyGaming * 7;
+
+// give user feedback
+console.log("\nGotcha. You want to spend at least " + dailyGaming + " hours a day playing Baldur's Gate. That comes out to " + leisureHours + " hours spent across seven daily gaming sessions.\n");
+
+// additional feedback based on whether they have exceeded total hours in a week
+if (necessities + leisureHours > hoursInWeek) {
+  console.log("I think you need to reconsider your priorities. There are only " + hoursInWeek + " hours in a week, and with all your other obligations you only have " + (hoursInWeek - necessities) + " hours throughout the week to spend on leisurely activities.");
 } else {
-    myPhrase = "Imagine having health insurance... ☹️";
+  console.log("Cool. That sounds like a balanced schedule. You'll have about " + (hoursInWeek -necessities + leisureHours) + " hours leftover to spend how you'd like.");
 }
-
-console.log("\nHello, " + myName + ". " + myPhrase);
