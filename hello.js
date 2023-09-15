dc571b64c86db5afda321977b794fb82ae5d4a7d
Vance <vancesutherland@hotmail.com>
2023-09-14 19:00:30 -0500

    hello

--- hello.js
+++ hello.js
@@ -11,6 +11,7 @@ let studyHours = 18;
 // create variable to reduce clutter
 let necessities = sleepHours + workHours + miscHours + studyHours;
 
+
 // instantiate hours spent gaming on teh daily, then multiply by 7 to update leisureHours
 let dailyGaming = Number(input.question("How many hours a day would you like to spend playing Baldur's Gate 3? "));
 let leisureHours = dailyGaming * 7;
@@ -22,5 +23,5 @@ console.log("\nGotcha. You want to spend at least " + dailyGaming + " hours a da
 if (necessities + leisureHours > hoursInWeek) {
   console.log("I think you need to reconsider your priorities. There are only " + hoursInWeek + " hours in a week, and with all your other obligations you only have " + (hoursInWeek - necessities) + " hours throughout the week to spend on leisurely activities.");
 } else {
-  console.log("Cool. That sounds like a balanced schedule. You'll have about " + (hoursInWeek -necessities + leisureHours) + " hours leftover to spend how you'd like.");
+  console.log("Cool. That sounds like a balanced schedule. You'll have about " + (hoursInWeek -(necessities + leisureHours)) + " hours leftover to spend how you'd like.");
 }
